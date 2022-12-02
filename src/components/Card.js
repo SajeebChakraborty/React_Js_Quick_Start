60205558f88980f0a5362235b03c76000ec1c08e
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2022-12-03 01:40:08 +0600

    sending parameter using Props

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,20 +3,21 @@ const date=new Date();
 
 const year=date.getFullYear();
 
-function Card()
+function Card(props)
 {
 
     return  <div className='card'>
 
         <h1 className='cardTitle'>
 
-        Ticket
+            { props.titleText }
 
         </h1>
 
         <p className='cardBody'>
 
-        In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.
+
+            { props.descText }
 
         </p>
 
