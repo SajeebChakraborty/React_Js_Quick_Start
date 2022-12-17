cb2baf443de8b7db2d896d64852da3a2353afa35
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2022-12-17 20:51:04 +0600

    error handling

--- src/components/FetchData/FetchData.js
+++ src/components/FetchData/FetchData.js
@@ -4,6 +4,7 @@ export default function FetchData() {
 
   const [todos,setTodos]=useState(null);
   const [isloading,setLoading]=useState(true);
+  const [error,setError]=useState(null);
 
   useEffect(()=>{
 
@@ -12,17 +13,35 @@ export default function FetchData() {
       fetch('https://jsonplaceholder.typicode.com/todos')
       .then((res)=>{
 
-          return res.json();
+          if(!res.ok)
+          {
+
+            throw Error("fetching is not successfull");
+
+          }
+          else
+          {
+
+            return res.json();
+
+          }
 
       })
       .then((data)=>{
 
           setTodos(data);
           setLoading(false);
+          setError(null);
+
+
+      })
+      .catch((error)=>{
+
+        setError(error.message);
+        setLoading(false);
 
 
       });
-
 
 
     },2000)
@@ -34,6 +53,8 @@ export default function FetchData() {
     <div>
       
       <h1>Fetch data from 'Fake API'</h1>
+
+      {error && error}
 
       {isloading && <p>it is loading</p>}
 
