772faf7fe766f87e27cbdbeb456d8421b14cd218
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-03-01 00:08:40 +0600

    state base dynamic styling

--- src/components/DynamicStyling/DynamicStyling.js
+++ src/components/DynamicStyling/DynamicStyling.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const DynamicStyling = () => {
 
-  const error = false;
+  const [name,setName] = useState("");
+  const [validInput,setValidInput] = useState(false);
+
+  useEffect(()=>{
+
+    if(name.length < 2)
+    {
+
+        setValidInput(false);
+
+    }
+    else
+    {
+
+        setValidInput(true);
+
+    }
+
+  },[name])
+
+  const handleChange = (e) => {
+
+    setName(e.target.value);
+
+
+  }
 
   return (
     <div>
       
-        <h1 style={{ color: error ? "red" : "green" }}>Welcome</h1>
+      <input type="text" value = {name} style={{ backgroundColor: validInput ? "green" : "red" }} onChange={handleChange} />
 
     </div>
   )
